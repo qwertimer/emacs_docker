8f0d7e1b92b4f19a479aa3bc0857a586f312606c
qwertimer1 <delltim@gmail.com>
2021-04-27 10:18:37 +1000

    Fixed issue with $PWD, new function is created instead of alias

--- install_emacs.sh
+++ install_emacs.sh
@@ -8,24 +8,28 @@ if ! type docker >/dev/null 2>&1; then
 fi
 #Run docker build of doom-emacs
 docker build -t doom-emacs -t qwertimer/doom-emacs emacs-docker/.
+
+
 #docker push qwertimer/doom-emacs
 
 ## Check if alias exists in bashrc-personal
 
 
 if [ -f ~/.bashrc-personal ]; then 
-    if grep "alias emacs" ~/.bashrc-personal
+    if grep "emacs()" ~/.bashrc-personal
     then
-       echo "alias already exists" 
+       echo "function already exists" 
     else 
-        echo 'alias emacs="docker run -it --rm -v $PWD:/tmp doom-emacs:latest"' >> ~/.bashrc-personal
+        
+        echo "emacs() {docker run -it --rm -v "$PWD:/tmp:Z" doom-emacs:latest}" >> ~/.bashrc-personal
     fi
 else
     echo "bashrc-personal doesn't exist adding it now"
     echo "Adding bashrc-personal as an extension to current bashrc"
 
     echo -n > ~/.bashrc-personal
-    echo 'alias emacs="docker run -it --rm -v $PWD:/tmp doom-emacs:latest"' >> ~/.bashrc-personal
+
+    echo "emacs() {docker run -it --rm -v "$PWD:/tmp:Z" doom-emacs:latest}" >> ~/.bashrc-personal
 fi
 
 ## re-source bashrc
